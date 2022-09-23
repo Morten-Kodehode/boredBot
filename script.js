ba2b7b3de7749c1ca2bec9b9bdf478a8f31ce8e6
Morten Johansson <morten1.kodehode@gmail.com>
2022-09-23 11:09:36 +0200

    added js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-// more stuff
+document.querySelector("button").addEventListener("click", () => {
+  fetch("https://apis.scrimba.com/bored/api/activity")
+    .then((resonse) => resonse.json())
+    .then((data) => {
+      document.querySelector("h4").textContent = data.activity;
+    });
+});
